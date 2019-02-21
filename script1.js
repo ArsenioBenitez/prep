72e64046d5ea0f65621e29c0504349d2908ff744
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-21 12:06:43 -0500

    jj

--- script1.js
+++ script1.js
@@ -1,7 +1,8 @@
 var nums = [11,4,41,50,7,19,8,18,2,6,43,6,20,43,2,23,12,24,31];
 var mean = 0;
 var mode = "2,6,43";
-var median = nums.sort()[(nums.length - 1) / 2];
+nums.sort();
+var median = nums[(nums.length - 1) / 2];
 nums.forEach(function(d){
   mean += d;
 });
