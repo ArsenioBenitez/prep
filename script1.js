40e33322b65eb595afe763b922e37cf07f7552e9
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-21 12:10:24 -0500

    j

--- script1.js
+++ script1.js
@@ -1,8 +1,7 @@
 var nums = [11,4,41,50,7,19,8,18,2,6,43,6,20,43,2,23,12,24,31];
 var mean = 0;
 var mode = "2,6,43";
-nums.sort();
-var median = nums[(nums.length - 1) / 2];
+var median = nums[(nums.length -1) / 2];
 nums.forEach(function(d){
   mean += d;
 });
